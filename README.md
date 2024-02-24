ffc3c3e54fa99d78496434fcd6beecc482a2624c
Evrayem3201b <154753963+Evrayem3201b@users.noreply.github.com>
2024-02-24 14:28:55 +0200

    Upload Portopholio

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Evrayem3201b.io
+
+This is a Landing page of mine that have all of my projects
+
+
+## 🚀 About Me
+I'm a Front-end developer that have 3 years of experience.
+
+
+
+
+
+## 🛠 Skills
+Javascript, HTML, CSS, React
+
+
+
+## ☎ Contacts
+betecno100@gmail.com
+
++20 01008303207
+
+Egypt, Sharm El Sheikh
+
---
+++ script - blogs.js
@@ -0,0 +1,62 @@
+let btn = document.querySelector('#bars');
+let nav = document.querySelector('.nav-container');
+let body = document.querySelector('#all-body');
+let myName = document.querySelector('.my-name');
+let navMain = document.querySelector('#navMain');
+let activeBtn = document.querySelector('#bars .active');
+let listHeader = document.querySelector('.list-header');
+let chevron = document.querySelector('#chevron');
+let detailsHeader = document.querySelector('.details-header');
+let underChevron = document.querySelector('.under-the-chevron');
+let modesSwitch = document.querySelector('.button-desktop');
+let modesSwitchDesk = document.querySelector('#check-desk');
+let modesSwitchMob = document.querySelector('#check');
+let modesSwitchMobButton = document.querySelector('.button');
+
+
+
+btn.onclick = function () {
+    btn.classList.toggle('active');
+    nav.classList.toggle('active');
+    navMain.classList.toggle('active');
+    listHeader.classList.toggle('active');
+    myName.classList.toggle('text');
+    body.classList.toggle('active-body');
+};
+
+modesSwitchDesk.onclick = function (){
+    body.classList.toggle('light');
+};
+
+modesSwitchMob.onclick = function (){
+    body.classList.toggle('light');
+};
+
+
+
+
+
+
+
+/*
+function closeNav(){
+    document.getElementById('navList').style.width = '52px';
+    document.getElementById('navContainer').style.display = 'none';
+};
+
+function openNav(){
+    document.getElementById('navList').style.width = 'auto';
+    document.getElementById('navContainer').style.display = 'block';
+};*/
+
+
+/*
+bars.addEventListener("click", function open(){
+    document.getElementById('navList').style.width = 'auto';
+    document.getElementById('navContainer').style.display = 'block';
+});
+
+bars.addEventListener("click", function close(){
+    document.getElementById('navList').style.width = '52px';
+    document.getElementById('navContainer').style.display = 'none';
+});*/
